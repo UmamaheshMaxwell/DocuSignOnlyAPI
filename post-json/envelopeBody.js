275ea45b1d23915af9266b269b1be1efe7d2cfb0
Vani <Vani>
2016-04-11 00:54:48 +0530

    Adds tab info of PDF file to encode

--- post-json/envelopeBody.js
+++ post-json/envelopeBody.js
@@ -1,42 +1,117 @@
 // Status should be 'created' stop E-mails
 var envelopeRequest = {
-      emailSubject: 'Maxwell Envelope Document',
-      documents: [
+    "emailSubject": "Please sign this document...",
+    "emailBlurb": "Sent from the DocuSign API",
+    "status": "sent",
+    "compositeTemplates": [
         {
-          name: 'Forms/MasterForm.pdf',
-          documentId: 1
-        },
-        {
-          name: 'Forms/TextTags_Example.pdf',
-          documentId: 2
-        }
-      ],
-      recipients: {
-        signers: [
-          {
-            email: 'maheshmeka16@gmail.com',
-            name: 'Umamaheswararao Meka',
-            recipientId: 1,
-            tabs: {
-              signHereTabs: [
+            "inlineTemplates": [
                 {
-                  xPosition: '100',
-                  yPosition: '100',
-                  documentId: '1',
-                  pageNumber: '1'
-                },
-                {
-                  xPosition: '100',
-                  yPosition: '600',
-                  documentId: '2',
-                  pageNumber: '1'
+                    "sequence": "1",
+                    "recipients": {
+                        "signers": [
+                            {
+                               "email": "maheshmeka16@gmail.com",
+                               "name": "Umamaheswararao Meka",
+                               "recipientId": "1",
+                               "defaultRecipient": "true",
+                               "tabs":{
+                                  "textTabs":[
+                                        {
+                                           "tabLabel":"primary|nameLast",
+                                           "value":"Umamaheswararao"
+                                        },
+                                        {
+                                           "tabLabel":"primary|nameFirst",
+                                           "value":"Meka"
+                                        },
+                                        {
+                                           "tabLabel":"primary|ssn", 
+                                           "value":"1234-123-1234"
+                                        },
+                                        {
+                                           "tabLabel":"primary|jobTitle",
+                                           "value":"Tech Lead"
+                                        },
+                                        {
+                                           "tabLabel":"employeeid",
+                                           "value":"VLSP1246"
+                                        },
+                                        {
+                                           "tabLabel":"birthplace",
+                                           "value":"India"
+                                        },
+                                        {
+                                           "tabLabel":"primary|addressCity",
+                                           "value":"Bangalore"
+                                        },
+                                        {
+                                           "tabLabel":"employer|businessName",
+                                           "value":"Healthcare"
+                                        },
+                                        {
+                                           "tabLabel":"family|dateHire",
+                                           "value":"22/09/2014"
+                                        },
+                                        {
+                                           "tabLabel":"employeeid",
+                                           "value":"VLSP1246"
+                                        }                                                                                                                                                                
+                                     ],
+                                   "radioGroupTabs":[
+                                        {
+                                           "groupName":"primary|gender",
+                                           "radios":[
+                                              {
+                                                 "value":"male",
+                                                 "selected":true
+                                              }
+                                           ],
+                                         },
+
+                                         {  
+                                           "groupName":"uscitizen",
+                                           "radios":[
+                                              {
+                                                 "value":"no",
+                                                 "selected":true
+                                              }
+                                           ]
+                                          },
+
+                                          { 
+                                           "groupName":"greencard",
+                                           "radios":[
+                                              {
+                                                 "value":"no",
+                                                 "selected":true
+                                              }
+                                           ]
+                                          },
+
+                                          {
+                                           "groupName":"visa",
+                                           "radios":[
+                                              {
+                                                 "value":"no",
+                                                 "selected":true
+                                              }
+                                           ]
+                                         }
+                                     ]
+                                  }                                
+                            }
+                        ]
+                    }
                 }
-              ]
+            ],
+            "document": {
+                "name": "Forms/MasterForm.pdf",
+                "documentId": "1",
+                "transformPdfFields": "true"
             }
-          }
-        ]
-      },
-      status: 'sent'
-    }
+        }
+    ]
+}
 
  module.exports = envelopeRequest;  
--- post-json/templateBody.js
+++ post-json/templateBody.js
@@ -21,8 +21,108 @@ var body = {// Status should be 'created' stop E-mails
                 folderUri: "sample string 12",
                 parentFolderUri: "sample string 13",
                 owner: {}
-              }
-              ,
+              },
+
+              recipients: {
+                  signers: 
+                  [
+                    {
+                      email: "maheshmeka16@gmail.com",
+                      name: "Umamaheswararao Meka",
+                      recipientId: "1",
+                      defaultRecipient: "true",
+                      tabs:{
+                        textTabs:[
+                              {
+                                 "tabLabel":"primary|nameLast",
+                                 "value":"Umamaheswararao"
+                              },
+                              {
+                                 "tabLabel":"primary|nameFirst",
+                                 "value":"Meka"
+                              },
+                              {
+                                 "tabLabel":"primary|dob",
+                                 "value":"04/16/1982"
+                              },
+                              {
+                                 "tabLabel":"primary|ssn", 
+                                 "value":"1234-123-1234"
+                              },
+                              {
+                                 "tabLabel":"primary|jobTitle",
+                                 "value":"Tech Lead"
+                              },
+                              {
+                                 "tabLabel":"employeeid",
+                                 "value":"VLSP1246"
+                              },
+                              {
+                                 "tabLabel":"birthplace",
+                                 "value":"India"
+                              },
+                              {
+                                 "tabLabel":"primary|addressCity",
+                                 "value":"Bangalore"
+                              },
+                              {
+                                 "tabLabel":"employer|businessName",
+                                 "value":"Healthcare"
+                              },
+                              {
+                                 "tabLabel":"family|dateHire",
+                                 "value":"22/09/2014"
+                              },
+                              {
+                                 "tabLabel":"employeeid",
+                                 "value":"VLSP1246"
+                              }                                                                                                                                                                
+                           ],
+                          radioGroupTabs:[
+                              {
+                                 "groupName":"primary|gender",
+                                 "radios":[
+                                    {
+                                       "value":"male",
+                                       "selected":true
+                                    }
+                                 ],
+                               },
+
+                               {  
+                                 "groupName":"uscitizen",
+                                 "radios":[
+                                    {
+                                       "value":"no",
+                                       "selected":true
+                                    }
+                                 ]
+                                },
+
+                                { 
+                                 "groupName":"greencard",
+                                 "radios":[
+                                    {
+                                       "value":"no",
+                                       "selected":true
+                                    }
+                                 ]
+                                },
+
+                                {
+                                 "groupName":"visa",
+                                 "radios":[
+                                    {
+                                       "value":"no",
+                                       "selected":true
+                                    }
+                                 ]
+                               }
+                           ]
+                        }                                                        
+                    }
+                  ]
+              },
 
               documents: [
                         {
